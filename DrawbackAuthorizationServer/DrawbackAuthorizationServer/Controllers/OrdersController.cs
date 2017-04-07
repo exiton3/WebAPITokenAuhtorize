331c0761a2d23e4ee9a96454347ecf98cad65d93
Sergey Kichuk <exiton3@gmail.com>
2017-04-07 18:56:04 +0300

    Initial commit

---
+++ DrawbackAuthorizationServer/DrawbackAuthorizationServer/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace DrawbackAuthorizationServer.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(Order.CreateOrders());
+        }
+
+    }
+   
+}
